0c1293c9ef9125c7449f1fbda5a5d0be232243ca
mirajul alam <alifmirajulalam@gmail.comm>
2022-02-28 23:44:21 +0600

    search card added

--- js/app.js
+++ js/app.js
@@ -4,5 +4,28 @@ const loadPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data))
+        .then(data => displayPhone(data.data))
+};
+
+const displayPhone = phones => {
+    console.log(phones)
+    const searchPhone = document.getElementById('search-result');
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `                 
+                        <div class="card">
+                            <div class="text-center">
+                            <img class="w-50" src="${phone.image}" class="card-img-top" alt="...">
+                            </div>
+                            <div class="card-body text-center">
+                            <h2>${phone.phone_name}</h2>
+                                <h3 class="card-title">${phone.brand}</h3>
+                                <p class="card-text"></p>
+                                <button onclick="loadPhoneById('${phone.slug}')">Details</button>
+                            </div>
+                        </div>
+        `;
+        searchPhone.appendChild(div);
+    });
 };
